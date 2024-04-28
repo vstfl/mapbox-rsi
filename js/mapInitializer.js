02ad9ed9924b3135840d9d3f4b1509adb4fb93a7
BuildTools <unconfigured@null.spigotmc.org>
2024-04-28 02:01:20 -0600

    Clean up console toggle transition and some css

--- js/mapInitializer.js
+++ js/mapInitializer.js
@@ -1,7 +1,9 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoidXJiaXp0b24iLCJhIjoiY2xsZTZvaXd0MGc4MjNzbmdseWNjM213eiJ9.z1YeFXYSbaMe93SMT6muVg';
             const map = new mapboxgl.Map({
                 container: 'map', 
-                style: 'mapbox://styles/urbizton/clve9aeu900c501rd7qcn14q6',
+                style: 'mapbox://styles/urbizton/clve9aeu900c501rd7qcn14q6', // Normal
+                // style: 'mapbox://styles/urbizton/clvj8ldr8014x01q1f0et6pay', // Satellite
+                // style: 'mapbox://styles/urbizton/clvj8hyu7014v01q18d11gsz5',
                 center: [-93.53, 41.99],
                 zoom: 6, 
                 maxZoom: 14,
